afeed8c48d6219918f90edc8e5532a14d93b4e4a
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2024-12-15 17:23:02 +0300

    create DreamController

---
+++ src/main/java/com/yapai/dreamcatcher/controller/DreamController.java
@@ -0,0 +1,25 @@
+package com.yapai.dreamcatcher.controller;
+
+import com.yapai.dreamcatcher.model.DreamInterpretation;
+import com.yapai.dreamcatcher.service.IDreamService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/dream")
+public class DreamController {
+
+    private final IDreamService dreamService;
+
+    public DreamController(IDreamService dreamService) {
+        this.dreamService = dreamService;
+    }
+
+    @GetMapping
+    public DreamInterpretation getDreamInterpretation(@RequestParam String dream) {
+        return dreamService.getDreamInterpretation(dream);
+    }
+
+}
