0c1d92ebc67cfe87f48bedc4bf54ebc57f38b5b6
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2024-12-15 16:43:19 +0300

    Initial commit

---
+++ src/test/java/com/yapai/dreamcatcher/DreamcatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yapai.dreamcatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamcatcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
