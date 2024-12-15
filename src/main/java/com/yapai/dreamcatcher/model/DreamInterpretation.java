17d2dc6a8ec4df24af5c653638e0ac88f89cbbd4
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2024-12-15 16:49:55 +0300

    create DreamInterpretation model class

---
+++ src/main/java/com/yapai/dreamcatcher/model/DreamInterpretation.java
@@ -0,0 +1,22 @@
+package com.yapai.dreamcatcher.model;
+
+public class DreamInterpretation {
+
+    private String content;
+
+    public DreamInterpretation() {
+    }
+
+    public DreamInterpretation(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+}
