e8dbf4828b7ff1c383cfd9bf30ffe945cd3f23ae
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2024-12-15 16:56:12 +0300

    create AppConstants

---
+++ src/main/java/com/yapai/dreamcatcher/utils/AppConstants.java
@@ -0,0 +1,5 @@
+package com.yapai.dreamcatcher.utils;
+
+public class AppConstants {
+    public static final String DREAM_PROMPT = "Kullanıcıdan gelen rüyaya en fazla 300 karakter içeren bir tabir yapan asistansın";
+}
