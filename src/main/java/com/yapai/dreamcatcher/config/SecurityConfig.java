822b045ce4d4a36ed5d43624d2eaccca089b36e8
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2024-12-15 22:38:01 +0300

    create SecurityConfig

---
+++ src/main/java/com/yapai/dreamcatcher/config/SecurityConfig.java
@@ -0,0 +1,48 @@
+package com.yapai.dreamcatcher.config;
+
+import com.yapai.dreamcatcher.service.IUserService;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import java.io.IOException;
+
+@Configuration
+public class SecurityConfig {
+
+    private final IUserService userService;
+
+    public SecurityConfig(IUserService userService) {
+        this.userService = userService;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(registry -> {
+                    registry.anyRequest().permitAll();
+                }).oauth2Login(oauth2login -> {
+                    oauth2login.successHandler(new AuthenticationSuccessHandler() {
+                        @Override
+                        public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+                            userService.addUser(authentication);
+                            authentication.getCredentials();
+                            response.sendRedirect("/home");
+                        }
+                    });
+                })
+                .formLogin(Customizer.withDefaults())
+                .build();
+    }
+
+}
