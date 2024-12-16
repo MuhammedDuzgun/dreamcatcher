c68cea4306edd1fb362a68160c9208b4ad67207f
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2024-12-16 16:43:12 +0300

    modify IUserRepository

--- src/main/java/com/yapai/dreamcatcher/repository/IUserRepository.java
+++ src/main/java/com/yapai/dreamcatcher/repository/IUserRepository.java
@@ -3,7 +3,9 @@ package com.yapai.dreamcatcher.repository;
 import com.yapai.dreamcatcher.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 
 public interface IUserRepository extends JpaRepository<User, Long> {
-    User findByEmail(String email);
+    Optional<User> findByEmail(String email);
 }
