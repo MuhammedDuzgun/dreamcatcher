2fb846e5114d523fd9cae3e4564ecdab45d4a3f0
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2024-12-16 16:47:11 +0300

    create ICommentService and its impl

---
+++ src/main/java/com/yapai/dreamcatcher/service/ICommentService.java
@@ -0,0 +1,10 @@
+package com.yapai.dreamcatcher.service;
+
+import com.yapai.dreamcatcher.dto.CommentDto;
+import com.yapai.dreamcatcher.dto.CreateCommentRequest;
+import org.springframework.security.core.Authentication;
+
+public interface ICommentService {
+    CommentDto addComment(Authentication authentication, CreateCommentRequest createCommentRequest);
+    void deleteComment(Authentication authentication, Long commentId);
+}
---
+++ src/main/java/com/yapai/dreamcatcher/service/impl/CommentService.java
@@ -0,0 +1,68 @@
+package com.yapai.dreamcatcher.service.impl;
+
+import com.yapai.dreamcatcher.dto.CommentDto;
+import com.yapai.dreamcatcher.dto.CreateCommentRequest;
+import com.yapai.dreamcatcher.entity.Comment;
+import com.yapai.dreamcatcher.entity.Dream;
+import com.yapai.dreamcatcher.entity.User;
+import com.yapai.dreamcatcher.mapper.CommentMapper;
+import com.yapai.dreamcatcher.repository.ICommentRepository;
+import com.yapai.dreamcatcher.repository.IDreamRepository;
+import com.yapai.dreamcatcher.repository.IUserRepository;
+import com.yapai.dreamcatcher.service.ICommentService;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CommentService implements ICommentService {
+
+    private final IUserRepository userRepository;
+    private final IDreamRepository dreamRepository;
+    private final ICommentRepository commentRepository;
+
+    public CommentService(IUserRepository userRepository, IDreamRepository dreamRepository, ICommentRepository commentRepository) {
+        this.userRepository = userRepository;
+        this.dreamRepository = dreamRepository;
+        this.commentRepository = commentRepository;
+    }
+
+    @Override
+    public CommentDto addComment(Authentication authentication, CreateCommentRequest createCommentRequest) {
+        //User
+        OAuth2AuthenticationToken token = (OAuth2AuthenticationToken) authentication;
+        OAuth2User oauth2User = token.getPrincipal();
+        String email = oauth2User.getAttribute("email");
+        Optional<User> user = userRepository.findByEmail(email);
+
+        //Dream
+        Optional<Dream> dream = dreamRepository.findById(createCommentRequest.getDreamId());
+
+        //Comment
+        Comment comment = new Comment();
+        comment.setComment(createCommentRequest.getComment());
+        comment.setDream(dream.get());
+        comment.setUser(user.get());
+
+        Comment savedComment = commentRepository.save(comment);
+
+        return CommentMapper.mapToCommentDto(savedComment);
+    }
+
+    @Override
+    public void deleteComment(Authentication authentication, Long commentId) {
+        //User
+        OAuth2AuthenticationToken token = (OAuth2AuthenticationToken) authentication;
+        OAuth2User oauth2User = token.getPrincipal();
+        String email = oauth2User.getAttribute("email");
+        Optional<User> user = userRepository.findByEmail(email);
+
+        Optional<Comment> commentToDelete = commentRepository.findById(commentId);
+        if (commentToDelete.get().getUser().getId().equals(user.get().getId())) {
+            commentRepository.delete(commentToDelete.get());
+        }
+    }
+}
