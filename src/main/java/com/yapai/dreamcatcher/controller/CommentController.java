ee68e83959f442bf2e88f6718e2445d805cdd80d
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2024-12-16 16:47:45 +0300

    create CommentController

---
+++ src/main/java/com/yapai/dreamcatcher/controller/CommentController.java
@@ -0,0 +1,34 @@
+package com.yapai.dreamcatcher.controller;
+
+import com.yapai.dreamcatcher.dto.CommentDto;
+import com.yapai.dreamcatcher.dto.CreateCommentRequest;
+import com.yapai.dreamcatcher.service.ICommentService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/comment")
+public class CommentController {
+
+    private final ICommentService commentService;
+
+    public CommentController(ICommentService commentService) {
+        this.commentService = commentService;
+    }
+
+    @PostMapping("/add-comment")
+    public ResponseEntity<CommentDto> addComment(Authentication authentication,
+                                                 @RequestBody CreateCommentRequest createCommentRequest) {
+        CommentDto commentDto = commentService.addComment(authentication, createCommentRequest);
+        return ResponseEntity.ok(commentDto);
+    }
+
+    @DeleteMapping("/delete-comment/{commentId}")
+    public ResponseEntity<String> deleteComment(Authentication authentication,
+                                                @PathVariable("commentId") Long commentId) {
+        commentService.deleteComment(authentication, commentId);
+        return ResponseEntity.ok("Comment deleted");
+    }
+
+}
