9de38e133b1561166c19779b9a721085a9a28b42
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2024-12-17 17:05:04 +0300

    add findByUser method

--- src/main/java/com/yapai/dreamcatcher/repository/IDreamRepository.java
+++ src/main/java/com/yapai/dreamcatcher/repository/IDreamRepository.java
@@ -1,7 +1,11 @@
 package com.yapai.dreamcatcher.repository;
 
 import com.yapai.dreamcatcher.entity.Dream;
+import com.yapai.dreamcatcher.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface IDreamRepository extends JpaRepository<Dream, Long> {
+    List<Dream> findByUser(User user);
 }
