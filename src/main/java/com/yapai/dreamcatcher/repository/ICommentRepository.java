05ddb4b3222531b31bbd6840e569ed50799b6bc9
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2024-12-17 17:06:30 +0300

    add findByDream method

--- src/main/java/com/yapai/dreamcatcher/repository/ICommentRepository.java
+++ src/main/java/com/yapai/dreamcatcher/repository/ICommentRepository.java
@@ -1,8 +1,11 @@
 package com.yapai.dreamcatcher.repository;
 
 import com.yapai.dreamcatcher.entity.Comment;
+import com.yapai.dreamcatcher.entity.Dream;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ICommentRepository extends JpaRepository<Comment, Long> {
+import java.util.List;
 
+public interface ICommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findByDream(Dream dream);
 }
